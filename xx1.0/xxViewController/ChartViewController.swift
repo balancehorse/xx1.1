641e9fdafcc742eed5f43ff80d3f389bef78a14e
balance <1178966810@qq.com>
2019-04-13 17:16:47 +0800

    第二个页面

--- xx1.0/xxViewController/ChartViewController.swift
+++ xx1.0/xxViewController/ChartViewController.swift
@@ -9,8 +9,38 @@
 import UIKit
 import AAInfographics
 
-class ChartViewController: UIViewController {
+extension UIColor {
+    
+    // Hex String -> UIColor
+    convenience init(hexString: String) {
+        let hexString = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        
+        if hexString.hasPrefix("#") {
+            scanner.scanLocation = 1
+        }
+        
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: 1)
+    }}
 
+
+class ChartViewController: UIViewController {
+    
+    let backgView = UIView()
+    let items = ["7天","30天","365天"]
+    var segmented = UISegmentedControl()
     var ChartBool :Bool = true
     let chartViewWidth:CGFloat = UIScreen.main.applicationFrame.size.width
     let chartViewHeight:CGFloat = UIScreen.main.applicationFrame.size.height - 380
@@ -58,10 +88,10 @@ class ChartViewController: UIViewController {
                     .innerSize("20%")//内部圆环半径大小占比(内部圆环半径/扇形图半径),
                     .allowPointSelect(false)
                     .data([
-                        ["吃饭"  ,67],
-                        ["购物",999],
-                        ["社交",83],
-                        ["出行",11],
+                        ["吃饭"  ,124],
+                        ["购物",400],
+                        ["社交",120],
+                        ["出行",56],
                         
                         ])
                     .toDic()!,
@@ -77,8 +107,7 @@ class ChartViewController: UIViewController {
         titleLabel.textColor = UIColor.black
         self.navigationItem.titleView = titleLabel
         
-        let items = ["7天","30天","365天"]
-        let segmented = UISegmentedControl(items: items)
+        segmented = UISegmentedControl(items: items)
         //设置中心位置
         segmented.center = self.view.center
         segmented.frame = CGRect(x: 75, y: 10, width: 220, height: 22)
@@ -118,7 +147,90 @@ class ChartViewController: UIViewController {
             aaChartView.aa_drawChartWithChartModel(chartModel2)
         }
         //创建下半部分view
-        
+        showSecondView()
+    }
+    
+    func showSecondView() -> Void {
+
+        for v in backgView.subviews{
+            v.removeFromSuperview()
+        }
+        var imgArr = ["吃饭","社交","出行","购物"]
+        var booksArr = ["吃饭","社交","出行","购物"]
+        var percentArr = [0.6,0.75,0.8,0.5]
+        var costArr = ["200","400","600","350"]
+        var costPerArr = ["60%","75%","80%","50%"]
+        var colorArr = ["#fe117c","#ffc069","#06caf4","#7dffc0"]
+        backgView.frame = CGRect(x: 0, y: 300, width: UIScreen.main.applicationFrame.size.width, height: 400)
+        if ChartBool{
+        backgView.backgroundColor = UIColor(red: 184/255.0, green: 184/255.0, blue: 173/255.0, alpha: 0.5)
+        let titleLabel = UILabel.init(frame: CGRect(x: 5, y: 2, width: 100, height: 17))
+        titleLabel.text = "开支详情"
+        titleLabel.font = UIFont.systemFont(ofSize: 16)
+        titleLabel.textColor = UIColor(red: 68/255.0, green: 68/255.0, blue: 68/255.0, alpha: 1)
+        backgView.addSubview(titleLabel)
+        self.view.addSubview(backgView)
+        for index in 0..<4{
+            let backgroundView = UIView.init(frame: CGRect(x: 0 , y: 20 + index * 50, width: 475, height: 50))
+            backgroundView.backgroundColor = UIColor.white
+            let lineView =  UIView.init(frame: CGRect(x: 45 , y: 20 + index * 50, width: 475, height: 1))
+            lineView.backgroundColor = UIColor(red: 184/255.0, green: 184/255.0, blue: 173/255.0, alpha: 0.75)
+            let imgView = UIImageView.init(frame: CGRect(x: 18 , y: 30 + index*50, width: 28, height: 28))
+            imgView.image = UIImage(named: imgArr[index])
+            let bookName = UILabel.init(frame: CGRect(x: 56, y: 27 + index * 50, width: 50, height: 20))
+            bookName.text = booksArr[index]
+            bookName.font = UIFont.systemFont(ofSize: 14)
+            
+            //生成进度条
+            let balanceView = UIProgressView.init(frame: CGRect(x: 58, y: 57 + index * 50, width: Int(UIScreen.main.applicationFrame.size.width/1.35), height: 15))
+            
+            balanceView.progress = Float(percentArr[index])
+            balanceView.progressTintColor = UIColor.init(red: 34/255.0, green: 162/255.0, blue: 221/255.0, alpha: 1) //进度颜色
+            balanceView.trackTintColor = UIColor.init(red: 184/255.0, green: 184/255.0, blue: 173/255.0, alpha: 1) //剩余进度颜色
+            balanceView.layer.masksToBounds = true
+            balanceView.layer.cornerRadius = 2.5
+            balanceView.transform = CGAffineTransform(scaleX: 1.0, y: 3.0)
+            
+            let costMoney = UILabel.init(frame: CGRect(x: 340, y: 27 + index * 50, width: 50, height: 20))
+            costMoney.text = costArr[index]
+            costMoney.font = UIFont.systemFont(ofSize: 14)
+            costMoney.textColor = UIColor.init(red: 111/255.0, green: 111/255.0, blue: 111/255.0, alpha: 1)
+            
+            let costPercent = UILabel.init(frame: CGRect(x: 340, y: 48 + index * 50, width: 50, height: 20))
+            costPercent.text = costPerArr[index]
+            costPercent.font = UIFont.systemFont(ofSize: 14)
+            costPercent.textColor = UIColor.init(red: 111/255.0, green: 111/255.0, blue: 111/255.0, alpha: 1)
+            
+            backgView.addSubview(backgroundView)
+            if index != 0{
+                backgView.addSubview(lineView)}
+            backgView.addSubview(imgView)
+            backgView.addSubview(bookName)
+            backgView.addSubview(balanceView)
+            backgView.addSubview(costMoney)
+            backgView.addSubview(costPercent)
+            }}
+        else{
+            backgView.backgroundColor = UIColor.white
+            for index in 0..<4{
+                let blockView = UIView.init(frame: CGRect(x: 20 , y: 15 + index * 30, width: 10, height: 22))
+                blockView.backgroundColor = UIColor.init(hexString: costPerArr[index])
+                
+                
+                let bookName = UILabel.init(frame: CGRect(x: 40, y: 23 + index * 30, width: 50, height: 20))
+                bookName.text = booksArr[index]
+                bookName.font = UIFont.systemFont(ofSize: 14)
+                
+                let costPercent = UILabel.init(frame: CGRect(x: 75, y: 23 + index * 30, width: 50, height: 20))
+                costPercent.text = costPerArr[index]
+                costPercent.font = UIFont.systemFont(ofSize: 14)
+                costPercent.textColor = UIColor.init(red: 111/255.0, green: 111/255.0, blue: 111/255.0, alpha: 1)
+                backgView.addSubview(blockView)
+                backgView.addSubview(costPercent)
+                backgView.addSubview(bookName)
+            }
+        }
+        return
     }
     @objc func rightClick()->Void {
      
@@ -129,9 +241,13 @@ class ChartViewController: UIViewController {
             ChartBool = false
             navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named:"柱图"), style: .plain, target: self, action: #selector(self.leftClick))
             navigationItem.leftBarButtonItem?.tintColor = UIColor.gray
+            segmented.selectedSegmentIndex = 2
+            SegmentedChanged(segmented)
         }else{ ChartBool = true
             navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named:"饼图"), style: .plain, target: self, action: #selector(self.leftClick))
             navigationItem.leftBarButtonItem?.tintColor = UIColor.gray
+            segmented.selectedSegmentIndex = 2
+            SegmentedChanged(segmented)
         }
         
         viewWillAppear(true)
@@ -235,16 +351,91 @@ class ChartViewController: UIViewController {
             }
             
         }else{
-            if segmented.selectedSegmentIndex == 0{}
-            if segmented.selectedSegmentIndex == 1{}
-            if segmented.selectedSegmentIndex == 2{}
+            if segmented.selectedSegmentIndex == 0{
+                chartModel2 = AAChartModel()
+                    .chartType(.pie)
+                    .backgroundColor("#ffffff")
+                    .title("开销情况")
+                    .subtitle("virtual data")
+                    .dataLabelEnabled(true)//是否直接显示扇形图数据
+                    .yAxisTitle("℃")
+                    .series(
+                        [
+                            AASeriesElement()
+                                .name("Language market shares")
+                                .innerSize("20%")//内部圆环半径大小占比(内部圆环半径/扇形图半径),
+                                .allowPointSelect(false)
+                                .data([
+                                    ["吃饭"  ,67],
+                                    ["购物",200],
+                                    ["社交",83],
+                                    ["出行",45],
+                                    
+                                    ])
+                                .toDic()!,
+                        ]
+                )
+                viewWillAppear(true)
+            }
+            if segmented.selectedSegmentIndex == 1{
+                chartModel2 = AAChartModel()
+                    .chartType(.pie)
+                    .backgroundColor("#ffffff")
+                    .title("开销情况")
+                    .subtitle("virtual data")
+                    .dataLabelEnabled(true)//是否直接显示扇形图数据
+                    .yAxisTitle("℃")
+                    .series(
+                        [
+                            AASeriesElement()
+                                .name("Language market shares")
+                                .innerSize("20%")//内部圆环半径大小占比(内部圆环半径/扇形图半径),
+                                .allowPointSelect(false)
+                                .data([
+                                    ["吃饭"  ,124],
+                                    ["购物",400],
+                                    ["社交",120],
+                                    ["出行",56],
+                                    
+                                    ])
+                                .toDic()!,
+                        ]
+                )
+                viewWillAppear(true)
+            }
+            if segmented.selectedSegmentIndex == 2{
+                chartModel2 = AAChartModel()
+                    .chartType(.pie)
+                    .backgroundColor("#ffffff")
+                    .title("开销情况")
+                    .subtitle("virtual data")
+                    .dataLabelEnabled(true)//是否直接显示扇形图数据
+                    .yAxisTitle("℃")
+                    .series(
+                        [
+                            AASeriesElement()
+                                .name("Language market shares")
+                                .innerSize("20%")//内部圆环半径大小占比(内部圆环半径/扇形图半径),
+                                .allowPointSelect(false)
+                                .data([
+                                    ["吃饭"  ,600],
+                                    ["购物",800],
+                                    ["社交",157],
+                                    ["出行",100],
+                                    
+                                    ])
+                                .toDic()!,
+                        ]
+                )
+            }
+            viewWillAppear(true)
         }
-        //打印选项的索引
-        print("index is \(segmented.selectedSegmentIndex)")
-        //打印选择的文字
-        print("option is \(String(describing: segmented.titleForSegment(at: segmented.selectedSegmentIndex)))")//将获得值转为String类型
+//        //打印选项的索引
+//        print("index is \(segmented.selectedSegmentIndex)")
+//        //打印选择的文字
+//        print("option is \(String(describing: segmented.titleForSegment(at: segmented.selectedSegmentIndex)))")//将获得值转为String类型
     }
-    
+ 
  
     /*
     // MARK: - Navigation
--- xx1.0/xxViewController/DetailViewController.swift
+++ xx1.0/xxViewController/DetailViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource {
+    //测试用账本
+    var books = ["吃饭","出行","购物","社交"]
+    
+    //账本金额
+    var moneyID = ["500","500","400","300",]
+    
+    //图片名称
+    var imgName = ["吃饭","出行","购物","社交"]
+    
+    let contents:Dictionary<String,[String]> =
+        ["03月18日 星期一":["购物"],
+         "03月19日 星期二":["吃饭","出行","购物","交际"],
+         "03月20日 星期三":["吃饭","吃饭"]]
+    var keys:[String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //设置navigation的titile
@@ -20,18 +35,113 @@ class DetailViewController: UIViewController {
         titleLabel.textColor = UIColor.black
         self.navigationItem.titleView = titleLabel
         
-
+        navigationController?.isNavigationBarHidden = false
+        // 把字典里的key拿出来放到一个数组中，备用，作为章节的标题
+        keys = contents.keys.sorted()
+        
+        let tableView = UITableView(frame: CGRect(x: 0, y: 60, width: UIScreen.main.bounds.width
+            , height: UIScreen.main.bounds.height), style: .plain)
+        //        tableView.backgroundColor = UIColor.white;
+        //去掉没有数据显示部分多余的分隔线
+        tableView.tableFooterView = UIView.init(frame: CGRect.zero)
+        //将分隔线offset设为零，即将分割线拉满屏幕
+        tableView.separatorInset = UIEdgeInsets(top: 45, left: 45, bottom: 45, right: 0)
+        self.view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        //给TableView添加表头页眉
+        let headerView:UIView = UIView(frame:
+            CGRect(x:0, y:0, width:tableView.frame.size.width, height:60))
+        let headerlabel:UILabel = UILabel(frame: CGRect(x: 10, y: 5, width: tableView.frame.size.width, height: 20))
+        headerlabel.textColor = UIColor.darkGray
+        headerlabel.font = UIFont.systemFont(ofSize: 13)
+        headerlabel.text = "2019年          预设（元）          支出（元）          剩余（元）"
+        headerView.addSubview(headerlabel)
+        
+        let selectMonthBtn:UIButton = UIButton(frame: CGRect(x: 10, y: 28, width: 50, height: 30))
+        //        selectMonthBtn.backgroundColor = UIColor.lightGray
+        selectMonthBtn.setTitle("04月", for: .normal)
+        selectMonthBtn.setTitleColor(UIColor.black, for: .normal)
+        headerView.addSubview(selectMonthBtn)
+        
+        self.view.addSubview(headerView)
+        
+    }
+    //MARK: UITableViewDataSource
+    //MARK: 章节的个数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return keys.count
+    }
+    //MARK: 某一章节cell个数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let arr = contents[keys[section]]
+        return (arr?.count)!
+    }
+    //MARK: 初始化cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "indexsCellId")
+        if cell==nil {
+            cell = UITableViewCell(style: .value1, reuseIdentifier: "indexsCellId")
+        }
+        let arr = contents[keys[indexPath.section]]
+        cell?.textLabel?.text = arr?[indexPath.row]
+        //        cell?.detailTextLabel?.text = "¥  " + moneyID[indexPath.row]
+        cell?.imageView?.image = UIImage(named: imgName[indexPath.row])
+        cell?.imageView?.frame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        cell?.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
+        cell?.accessoryView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 15))
+        let costlabel = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 15))
+        costlabel.text = "-100"
+        costlabel.textColor = UIColor.darkGray
+        costlabel.font = UIFont.systemFont(ofSize: 15)
+        costlabel.sizeToFit()
+        costlabel.textAlignment = NSTextAlignment.center
+        cell?.accessoryView?.addSubview(costlabel)
+        
+        
+        
+        return cell!
+    }
+    //MARK: UITableViewDelegate
+    // 设置cell高度
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
+    }
+    //MARK: 每一个章节的标题
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return keys[section]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
     }
-    */
-
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.init(red: 237/255.0, green: 237/255.0, blue: 237/255.0, alpha: 1)
+        let titleLabel = UILabel(frame: CGRect(x: 10, y: 0, width: 150, height: 20))
+        titleLabel.text = keys[section]
+        titleLabel.textColor = UIColor.darkGray
+        titleLabel.sizeToFit()
+        titleLabel.font = UIFont.systemFont(ofSize: 13)
+        let costlabel = UILabel(frame: CGRect(x: tableView.frame.size.width-75, y: 0, width: 75, height: 20))
+        // 一天内共花了多少钱
+        costlabel.text = "支出：130"
+        costlabel.textColor = UIColor.darkGray
+        costlabel.sizeToFit()
+        costlabel.font = UIFont.systemFont(ofSize: 13)
+        //      titleLabel.center = CGPoint(x: self.view.frame.width/2, y: 20)
+        headerView.addSubview(titleLabel)
+        headerView.addSubview(costlabel)
+        return headerView
+        //        var label1 = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
+        //        label1.text = keys[section]
+        //        return label1
+    }
+    
 }
+
+
+
+
